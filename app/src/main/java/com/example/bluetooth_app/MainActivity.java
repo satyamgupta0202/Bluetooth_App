3b2a1f2e15b0ba2943cefcacc6df63e332f14bc3
satyam <2019335@iiitdmj.ac.in.com>
2021-03-01 17:06:19 +0530

    General Layout Set

--- app/src/main/java/com/example/bluetooth_app/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_app/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.bluetooth_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView status;
+    ListView listView;
+    Button search;
+    BluetoothAdapter bluetooth;
+    public void searchclicked (View view){
+        status.setText("Searching.........");
+        search.setEnabled(false);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.list_view);
+        status = findViewById(R.id.status);
+        search = findViewById(R.id.search);
     }
 }
